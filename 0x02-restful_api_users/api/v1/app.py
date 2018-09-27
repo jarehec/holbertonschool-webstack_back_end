82695e8c1bd3d6a8740c5838b70cfc6a47359094
jarehec <129@holbertonschool.com>
2018-09-27 03:04:00 +0000

    stat route

--- 0x02-restful_api_users/api/v1/app.py
+++ 0x02-restful_api_users/api/v1/app.py
@@ -5,6 +5,7 @@ module containing flask app
 import os
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import db_session
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -17,6 +18,12 @@ port = os.getenv('HBNB_API_PORT')
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({'error': 'Not found'}), 404
+
+
+@app.teardown_appcontext
+def teardown(bepis):
+    """ close the database connection after request is done """
+    db_session.remove()
 
 if __name__ == '__main__':
     app.run(host=host, port=int(port))
--- 0x02-restful_api_users/api/v1/views/index.py
+++ 0x02-restful_api_users/api/v1/views/index.py
@@ -4,9 +4,18 @@ module containing status handler
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import db_session
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ status handler """
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ stats handler """
+    user_count = len(db_session.query(User).all())
+    return jsonify({'users': user_count})
