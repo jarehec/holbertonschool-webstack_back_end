81f509b164b803a3db0c6b1a816e8b6f55f4d149
jarehec <bone1198@gmail.com>
2018-12-09 21:07:41 -0800

    add 8-all.py

---
+++ 0x04-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+module containing list_all function
+"""
+
+def list_all(mongo_collection):
+    """ lists all documents in a collection """
+    return [i for i in mongo_collection.find()]
